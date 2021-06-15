d233903172f20ba688f5555eb411470fddadc5fb
AudebertAdrien <audebertadrien.pro@gmail.com>
2021-06-15 20:47:54 +0200

    start

--- src/navigation/RouterConfig.jsx
+++ src/navigation/RouterConfig.jsx
@@ -6,7 +6,6 @@ function RouterConfig() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-
       <Route path="*" render={() => <h1>404: page not found</h1>} />
     </Switch>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,17 @@
+import React from "react";
 import axios from "axios";
-import { useState, useEffect } from "react";
-
-import FranceDepartment from "../components/FranceDepartments";
-import DateChange from "../components/Date";
-
-import { format } from "date-fns";
-// let newDateFormat = format(new Date(), `yyyy-dd-MM`);
 
 function Home() {
-  const [dataCovid19, setDataCovid19] = useState([]);
-  /* WEBPACK_BASE_URL is a global variable created at compile time by webpack 
-  https://data-gouv-server.herokuapp.com/ or http://localhost:3000/
-  */
-  let today = new Date();
-  let lastWeek = format(
-    new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7),
-    `yyyy-MM-dd`
-  );
+  axios
+    .get("http://localhost:3000/")
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-  useEffect(() => {
-    axios
-      .post(WEBPACK_BASE_URL, lastWeek, {
-        headers: { "content-type": "text/plain; charset=UTF-8" },
-      })
-      .then((res) => {
-        setDataCovid19(res.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div className="container">
-      <div className="row">
-        <FranceDepartment dataCovid19={dataCovid19} />
-      </div>
-      <div className="row my-4">
-        <p className="text-center">
-          Lastest data known on{" "}
-          <span className="lastWeekMessage">{lastWeek}</span>
-        </p>
-      </div>
-      <div className="row">
-        <div className="col-sm-6 d-flex justify-content-center">
-          <DateChange setDataCovid19={setDataCovid19} />
-        </div>
-        <div className="col-sm-6">
-          <ul className="list-group">
-            <li className="list-group-item incidence0">Incidence close to 0</li>
-            <li className="list-group-item incidence1">
-              Incidence less than 15%
-            </li>
-            <li className="list-group-item incidence2">
-              Incidence between 15% and 30%
-            </li>
-            <li className="list-group-item incidence3">
-              Incidence greater or equal to 30 %
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
