9b1ff6cdc7925f12add457a214ad3cb4fa8bd1ea
Adrien <audebertadrien.pro@gmail.com>
2021-07-02 13:54:44 +0200

    preset-react babel

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 module.exports = {
-  presets: [["@babel/preset-env"], ["@babel/react", { runtime: "automatic" }]],
+  presets: [
+    ["@babel/preset-env"],
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
   plugins: plugins,
 };
